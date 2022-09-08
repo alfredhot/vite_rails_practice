9ee0df16df20385f39c020914444743b2d644ddf
alfredhot <alfredhot@gmail.com>
2022-09-08 14:02:42 +0900

    try jquery

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
         alias: {
             'vue': 'vue/dist/vue.esm-bundler.js',
             "@": path.resolve(__dirname, 'app/'),
+            '$': 'jquery',
         }
     }
 })
